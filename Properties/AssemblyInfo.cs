5c1ea8d30a0f9e5e223b4fe350045014573eff36
Unstoppable <theunstoppable1000@gmail.com>
2024-07-28 03:24:20 +0300

    Upgrade to .NET 8.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TTFS to Mix Utility")]
-[assembly: AssemblyDescription("Utility to convert TTFS packages to Mix packages.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Unstoppable")]
-[assembly: AssemblyProduct("Ttfs2Mix")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("05c1df84-23c3-4a3d-bef6-d430cf3ae02d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
